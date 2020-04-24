38338dce07ef20339b8af040022fdf61f4efec20
Yuhai <yuhaibo@a-city.cn>
2020-04-24 13:11:47 +0800

    添加swagger注解

--- cloud-modules/cloud-demo-server/src/main/java/com/yhb/demo/CloudDemoApplication.java
+++ cloud-modules/cloud-demo-server/src/main/java/com/yhb/demo/CloudDemoApplication.java
@@ -4,7 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+@EnableSwagger2
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableGlobalMethodSecurity(prePostEnabled = true)
